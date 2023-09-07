0bccc6151855bd418047ba4e403e1fd40c008e0a
Jussi Maki <jussi@isovalent.com>
2023-09-07 10:33:02 +0300

    endpoint: Fix use of PolicyMapFullReconciliationInterval option
    [ upstream commit a883ef30c000dd5a877c90fc1c558547859a8c34 ]
    
    The last minute change in 867e41a610 to add an option for configuring the
    full endpoint policy sync forgot to actually change the use-site.
    
    Fixes: 867e41a610 ("endpoint: Only perform full synchronization periodically")
    Signed-off-by: Jussi Maki <jussi@isovalent.com>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/endpoint/bpf.go
+++ pkg/endpoint/bpf.go
@@ -1370,12 +1370,6 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 	return err
 }
 
-const (
-	// syncPolicyMapInterval is the interval for periodic full reconciliation of
-	// the policy map to catch unexpected discrepancies with agent and kernel state.
-	syncPolicyMapInterval = 15 * time.Minute
-)
-
 func (e *Endpoint) startSyncPolicyMapController() {
 	ctrlName := fmt.Sprintf("sync-policymap-%d", e.ID)
 	e.controllers.CreateController(ctrlName,
@@ -1390,7 +1384,7 @@ func (e *Endpoint) startSyncPolicyMapController() {
 				defer e.unlock()
 				return e.syncPolicyMapWithDump()
 			},
-			RunInterval: syncPolicyMapInterval,
+			RunInterval: option.Config.PolicyMapFullReconciliationInterval,
 			Context:     e.aliveCtx,
 		},
 	)
