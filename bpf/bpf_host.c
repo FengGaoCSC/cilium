25057bcd9d68ea7add2b3b29060a5816d9f3fc3c
Gilberto Bertin <jibi@cilium.io>
2023-09-13 12:05:31 +0200

    bpf: bpf_host: skip host firewall in cil_to_netdev if packet is SNATed
    when egress gateway traffic gets masqueraded, it can get redirected to a
    different interface that is also running bpf_host.
    
    When this happens, the host firewall logic should be skipped as it would
    be incorrect to enforce again any policy: after being masqueraded and
    redirected, the original pod identity is lost, and the host firewall
    would incorrectly identify this traffic as host or world
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1422,6 +1422,9 @@ int cil_to_netdev(struct __ctx_buff *ctx __maybe_unused)
 #endif
 
 #ifdef ENABLE_HOST_FIREWALL
+	if (ctx_snat_done(ctx))
+		goto skip_host_firewall;
+
 	if (!validate_ethertype(ctx, &proto)) {
 		ret = DROP_UNSUPPORTED_L2;
 		goto out;
@@ -1454,6 +1457,8 @@ out:
 	if (IS_ERR(ret))
 		return send_drop_notify_error_ext(ctx, 0, ret, ext_err,
 						  CTX_ACT_DROP, METRIC_EGRESS);
+
+skip_host_firewall:
 #endif /* ENABLE_HOST_FIREWALL */
 
 #if defined(ENABLE_BANDWIDTH_MANAGER)
@@ -1515,7 +1520,9 @@ out:
 	}
 #endif
 
-__maybe_unused exit:
+#ifdef ENABLE_HEALTH_CHECK
+exit:
+#endif
 	if (IS_ERR(ret))
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
